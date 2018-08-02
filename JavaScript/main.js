3c0ec7471fef0747eeb6dcf46d2e83d709d79cf1
WylkerGomes <wylkergomes@gmail.com>
2018-08-02 20:36:18 +0100

    first website eg

---
+++ JavaScript/main.js
@@ -0,0 +1,12 @@
+
+
+var linkClicked = document.getElementsByClassName('nav-link'); ///////https://codepen.io/RegisIrala/pen/EEwWVy
+var numClass = linkClicked.length;
+
+for (var i = 0; i < numClass; i++) {
+    linkClicked[i].addEventListener('click', function(){
+        var onTheMoment = document.getElementsByClassName('active');
+        onTheMoment[0].className = onTheMoment[0].className.replace(' active', '');
+        this.className += ' active'; // the active link becomes true
+    }, false);
+}
